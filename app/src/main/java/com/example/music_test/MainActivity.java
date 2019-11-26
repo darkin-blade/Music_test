239e05f0d2de70afecef91a69767cc180e8e8476
lynx <1197225628@qq.com>
2019-11-26 16:00:35 +0800

    first

---
+++ app/src/main/java/com/example/music_test/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.music_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public String appPath;
+    public MediaPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initApp();
+        test1();
+    }
+
+    public void initApp() {
+        // 检查权限
+        String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
+        int check_result = ActivityCompat.checkSelfPermission(this, permission);// `允许`返回0,`拒绝`返回-1
+        if (check_result != PackageManager.PERMISSION_GRANTED) {// 没有`写`权限
+            ActivityCompat.requestPermissions(this, new String[]{permission}, 1);// 获取`写`权限
+        }
+
+        // 初始化路径字符串
+        appPath = getExternalFilesDir("").getAbsolutePath();
+    }
+
+    public void test1() {
+        player = new MediaPlayer();
+        String musicPath = appPath + "/test2.wav";
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);// TODO 设置为音频
+        try {
+            File tmp = new File(musicPath);
+            if (tmp.exists()) {// 如果文件存在
+                player.setDataSource(musicPath);// TODO 异常
+                player.prepare();
+                player.start();
+            } else {
+                infoToast(this, musicPath + " not exists");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public void infoLog(String log) {
+        Log.i("fuck", log);
+    }
+
+    static public void infoToast(Context context, String log) {
+        Toast toast =  Toast.makeText(context, log, Toast.LENGTH_SHORT);
+        View view = toast.getView();
+        TextView textView = view.findViewById(android.R.id.message);
+        textView.setTextColor(Color.rgb(0x00, 0x00, 0x00));
+        toast.show();
+    }
+
+    @Override
+
+    public void onBackPressed() {
+        player.stop();// 停止
+        super.onBackPressed();
+    }
+}
