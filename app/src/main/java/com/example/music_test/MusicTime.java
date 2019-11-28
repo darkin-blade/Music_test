995d06b56db7ee9748d4f7da4cb3f2f4ee365631
lynx <2104934038@qq.com>
2019-11-28 09:35:03 +0800

    test:音乐时长

--- app/src/main/java/com/example/music_test/MusicTime.java
+++ app/src/main/java/com/example/music_test/MusicTime.java
@@ -3,9 +3,15 @@ package com.example.music_test;
 import android.app.Activity;
 import android.content.Context;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MusicTime {
     public Context myContext = null;
     public Activity myActivity = null;
+
+    public int total_time = 0;
+    public int cur_time = 0;
 
     public MusicTime() {
         ;
@@ -22,15 +28,21 @@ public class MusicTime {
             public void run() {
                 // 启动播放
                 MainPlayer.player.start();
+                total_time = MainPlayer.player.getDuration();
 
                 // 更新音乐进度
                 while (Thread.currentThread().isInterrupted() == false) {
                     try {
+                        // 每一秒更新一次
                         Thread.sleep(1000);
+                        cur_time = MainPlayer.player.getCurrentPosition();// 当前进度
+                        MainPlayer.infoLog("music playing: " + cur_time / 1000 + "/" + total_time / 1000);// TODO
+
                         myActivity.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                MainPlayer.infoLog("music playing");// TODO
+                                updateBar();// TODO 修改进度条
+                                updateTime();// TODO 修改当前进度
                             }
                         });
                     } catch (InterruptedException e) {
@@ -42,5 +54,23 @@ public class MusicTime {
                 }
             }
         }).start();
+    }
+
+    public void updateTime() {
+        SimpleDateFormat format = new SimpleDateFormat("mm:ss");
+
+        // 设置当前进度
+        Date tmp = new Date(cur_time);
+        String formatTime = format.format(tmp);
+        MainPlayer.curTime.setText(formatTime);
+
+        // TODO 更新总时长
+        tmp = new Date(total_time);
+        formatTime = format.format(tmp);
+        MainPlayer.totalTime.setText(formatTime);
+    }
+
+    public void updateBar() {
+        ;
     }
 }
