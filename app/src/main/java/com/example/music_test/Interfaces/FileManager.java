7b4b892cefd20cf28ed8b366fdfe4d1efcd4eaab
lynx <2104934038@qq.com>
2019-11-28 14:40:59 +0800

    temp:选择歌曲class

---
+++ app/src/main/java/com/example/music_test/Interfaces/FileManager.java
@@ -0,0 +1,137 @@
+package com.example.music_test.Interfaces;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.music_test.MainPlayer;
+import com.example.music_test.R;
+
+import java.io.File;
+
+public class FileManager extends DialogFragment {public int item_height = 130;
+    public int item_padding = 40;
+    public int type_padding = 20;
+    public int name_padding = 30;
+    public int name_margin = 10;
+
+    public View myView;
+    public TextView curPath;// 当前路径
+
+    @Override
+    public void show(FragmentManager fragmentManager, String tag) {
+        super.show(fragmentManager, tag);
+    }
+
+    @Override
+    public void onDismiss(final DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Activity activity = getActivity();
+        if (activity instanceof DialogInterface.OnDismissListener) {
+            ((DialogInterface.OnDismissListener) activity).onDismiss(dialog);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NO_FRAME, android.R.style.Theme);// 关闭背景(点击外部不能取消)
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    public void initPath() {
+        curPath = myView.findViewById(R.id.cur_path);// 路径框
+    }
+
+    public void initButton() {
+        ;
+    }
+
+    public void readPath(final String dirPath) {
+        // 特判根目录
+        if (dirPath == null) {
+            MainPlayer.infoToast(getContext(), "can't access this nameLibrary");// TODO
+            dismiss();// 强制返回
+            return;
+        }
+
+        // 清空并显示父目录
+        LinearLayout layout = myView.findViewById(R.id.item_list);
+        layout.removeAllViews();
+        createItem(2, "..", dirPath);// 父目录
+
+        // 遍历文件夹
+        File dir = new File(dirPath);
+        File[] items = dir.listFiles();
+        if (items != null) {
+            for (int i = 0; i < items.length; i++) {
+                if (items[i].isDirectory()) {
+                    createItem(1, items[i].getName(), dirPath);
+                } else {
+                    createItem(0, items[i].getName(), dirPath);
+                }
+            }
+        }
+
+        // 显示路径
+        curPath.setText(dirPath);// TODO 简化路径
+    }
+
+    public LinearLayout createItem(int itemType, final String itemName, final String itemPath) {// 创建图标
+        LinearLayout layout = myView.findViewById(R.id.item_list);
+        LinearLayout.LayoutParams itemParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, item_height);
+        LinearLayout.LayoutParams typeParam = new LinearLayout.LayoutParams(item_height, item_height);
+        LinearLayout.LayoutParams iconParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout.LayoutParams nameParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout item = new LinearLayout(getContext());// TODO 参数
+        item.setLayoutParams(itemParam);
+        item.setBackgroundResource(R.color.grey);
+
+        LinearLayout type = new LinearLayout(getContext());// 图标的外圈
+        type.setLayoutParams(typeParam);
+        type.setPadding(type_padding, type_padding, type_padding, type_padding);
+
+        View icon = new View(getContext());// 图标
+        icon.setLayoutParams(iconParam);
+        if (itemType == 0) {// 文件
+            icon.setBackgroundResource(R.drawable.item_file);
+        } else {// 文件夹
+            icon.setBackgroundResource(R.drawable.item_dir);
+        }
+
+        TextView name = new TextView(getContext());// 文件名
+        nameParam.setMargins(name_margin, name_margin, name_margin, name_margin);
+        name.setLayoutParams(nameParam);
+        name.setBackgroundResource(R.color.grey);
+        name.setText(itemName);
+        name.setPadding(name_padding, name_padding, name_padding, name_padding);
+        name.setSingleLine();
+
+        type.addView(icon);
+        item.addView(type);
+        item.addView(name);
+
+        item = itemOnClick(itemType, itemName, itemPath, item);
+
+        layout.addView(item);
+
+        return item;
+    }
+
+    public LinearLayout itemOnClick(int itemType, final String itemName, final String itemPath, LinearLayout item) {// 绑定点击事件
+        return item;
+    }
+}
